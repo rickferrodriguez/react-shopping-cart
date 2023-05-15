09b37f7f3953052b07dffde69b8e2f2536afaf4d
Richard Rodriguez <richardfernandorodriguez@gmail.com>
2023-05-14 22:18:29 -0500

    desktop view

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,47 +4,46 @@ import { AddToCartIcon, RemoveFromCartIcon } from './Icons'
 export function ItemProducts ({ product, addToCart, isProductInCart, removeFromCart }) {
   const { price, title, thumbnail, description, brand } = product
   return (
-    <li className='flex flex-col px-3 py-6 gap-3 bg-sky-900 rounded-3xl'>
-      <strong className='font-amulya text-center text-xl'>{title}</strong>
+    <li className='grid pb-4 overflow-hidden gap-3 bg-sky-900 rounded-3xl'>
       <img
-        className='w-full aspect-[4/3] object-cover'
+        className='w-full aspect-square object-cover'
         src={thumbnail}
         alt={title}
       />
-      <section>
+      <section className='px-3 flex flex-col'>
         <div className='grid'>
           <small className='font-amulya text-lg text-gray-400'>{brand}</small>
           <strong className='font-amulya text-2xl'>{title}</strong>
         </div>
-        <p className='line-clamp-3 text-gray-400 font-bold text-sm'>
+        <p className='line-clamp-3 text-gray-400 font-bold text-sm border-b-[1px] border-gray-400 mb-3'>
           {description}
         </p>
-      </section>
-      <footer className='flex justify-between border-t-[1px] border-gray-400 pt-3'>
-        <div className='grid'>
-          <small className='text-sm font-amulya text-gray-400'>Price</small>
-          <span className='text-2xl font-amulya'>
-            $ {price}.<small>00</small>{' '}
-          </span>
-        </div>
-        <button
-          onClick={isProductInCart ? removeFromCart : addToCart}
-          className={`flex items-center gap-3 px-3 rounded-3xl ${
+        <footer className='flex justify-between h-[54px] justify-self-end'>
+          <div className='grid'>
+            <small className='text-sm font-amulya text-gray-400'>Price</small>
+            <span className='text-2xl font-amulya'>
+              $ {price}.<small>00</small>{' '}
+            </span>
+          </div>
+          <button
+            onClick={isProductInCart ? removeFromCart : addToCart}
+            className={`flex items-center gap-3 px-3 rounded-3xl ${
             isProductInCart ? 'bg-red-700 hover:bg-red-800' : 'bg-sky-400 hover:bg-sky-600'
           } font-amulya  hover:cursor-pointer`}
-        >
-          <span>
-            {isProductInCart
-              ? <RemoveFromCartIcon />
-              : <AddToCartIcon />}
-          </span>
-          {
+          >
+            <span>
+              {isProductInCart
+                ? <RemoveFromCartIcon />
+                : <AddToCartIcon />}
+            </span>
+            {
             isProductInCart
               ? 'Remove'
               : 'Add to Cart'
           }
-        </button>
-      </footer>
+          </button>
+        </footer>
+      </section>
     </li>
   )
 }
@@ -54,11 +53,11 @@ export function Products ({ products }) {
   const checkProductInCart = (product) => cart?.some(item => item.id === product.id)
   const hasProducts = products?.length > 0
   return (
-    <main className='flex justify-center'>
+    <main className='flex justify-center w-full'>
       {
       hasProducts
         ? (
-          <ul className='grid grid-cols-1 w-[300px] gap-8'>
+          <ul className='grid grid-cols-1 w-[300px] gap-8 lg:grid-cols-4 lg:w-full'>
             {
         products?.map(product => {
           const isProductInCart = checkProductInCart(product)
